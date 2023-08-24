38527149ac2515c4c30532a6f81edcc1b8e2d6c6
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-08-24 23:34:30 +0300

    Frontend <> edited the calendar list style

--- recipes-frontend/src/components/CalendarList/index.jsx
+++ recipes-frontend/src/components/CalendarList/index.jsx
@@ -1,36 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { format } from 'date-fns';
+import { sendRequest } from '../../core/config/request';
+import { requestMethods } from '../../core/enums/requestMethods';
 
-function CalendarList({events}) {
-    
-
+function CalendarList({ events, fetchData }) {
     if (!events) {
         return <p>Loading events...</p>;
     }
 
-    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const handleDelete = async (eventId) => {
+        try {
+            const response = await sendRequest({
+                route: `/user/calendar/events/${eventId}`,
+                method: requestMethods.DELETE,
+            });
+
+            if (response.message === 'Event deleted successfully') {
+                fetchData(); // Fetch updated data after deleting the event
+            }
+        } catch (error) {
+            console.error('Failed to delete event:', error);
+        }
+    };
 
     return (
         <div className="table-container">
             <table id="calendarTable">
                 <thead>
                     <tr>
-                        <th>Day of Week</th>
+                        <th>Date</th>
                         <th>Recipe Name</th>
                         <th>Recipe Cuisine</th>
                     </tr>
                 </thead>
                 <tbody id="calendarBody">
-                {events.map((event, index) => (
-                    <tr key={index}>
-                        <td>{daysOfWeek[event.day_of_week - 1]}</td>
-                        <td>{event.recipe.name}</td>
-                        <td>{event.recipe.cuisine}</td>
-                    </tr>
-                ))}
+                    {events.map((event, index) => (
+                        <tr key={index}>
+                            <td>{format(new Date(event.event_date), 'MMMM do, yyyy')}</td>
+                            <td>{event.recipe.name}</td>
+                            <td>{event.recipe.cuisine}</td>
+                            <td className='delete-btn'>
+                                <button onClick={() => handleDelete(event.id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
     );
 }
 
-export default CalendarList
+export default CalendarList;
